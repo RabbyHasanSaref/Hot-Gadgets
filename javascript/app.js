1014d898fd5eac55062e586542affb7f8ba3a690
RabbyHasanSaref <rabbyhasansaref83@gmail.com>
2024-02-28 14:35:26 +0600

    All complete

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,12 @@
 // rest api data load
-const loadPhone = async (searchBar) => {
+const loadPhone = async (searchBar= 13, isShowAll) => {
         const res = await  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchBar}`);
         const data = await res.json();
         const phone = data.data;
-        loadPhoneShow(phone);
+        loadPhoneShow(phone, isShowAll);
 }
 
-const loadPhoneShow = (phone) => {
+const loadPhoneShow = (phone, isShowAll) => {
     const productContaine = document.getElementById('product-container');
 
     // same product repiting stop ----------------
@@ -14,7 +14,7 @@ const loadPhoneShow = (phone) => {
 
     // display multiplue product show button -----------
     const showBtn = document.getElementById('showbutton');
-    if(phone.length > 10){
+    if(phone.length > 10 && !isShowAll){
         showBtn.classList.remove('hidden');
     }
     else{
@@ -22,7 +22,9 @@ const loadPhoneShow = (phone) => {
     }
 
     // display product show system --------------------
-    phone = phone.slice(0, 6);
+    if(!isShowAll){
+        phone = phone.slice(0, 6);
+    }
 
     // product data load and display show
     phone.forEach(phones => {
@@ -34,7 +36,7 @@ const loadPhoneShow = (phone) => {
                 <h2 class="card-title">${phones.phone_name}</h2>
                 <p class="text-center">There are many variations of passages of available, but the majority have suffered</p>
                 <div class="card-actions justify-center">
-                    <button class="btn btn-primary">Show Details</button>
+                    <button onclick="showModals('${phones.slug}'); my_modal_1.showModal() "class="btn btn-primary">Show Details</button>
                 </div>
             </div>
         `
@@ -45,18 +47,19 @@ const loadPhoneShow = (phone) => {
     toggole(false);
 }
 
+
 // -------------------------------------------------------
 
 
 // search button 
-const searchBtn = () => {
+const searchBtn = (isShowAll) => {
     // loader Data function call
     toggole(true);
 
 
     const searchBar = document.getElementById('searchBar');
     const searchValue = searchBar.value;
-    loadPhone(searchValue);
+    loadPhone(searchValue, isShowAll);
 }
 
 // loader data
@@ -70,5 +73,55 @@ const toggole = (istoggole) => {
    }
 }
 
+// show modal 
+const showModals = async (id) => {
+    console.log('Hello World', id)
 
-// loadPhone()
+    // modal data load api 
+        const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+        const modal = await res.json();
+        const result = modal.data;
+        modalDataShow(result)
+}
+
+const modalDataShow = (result) => {
+    console.log(result)
+    const phoneName = document.getElementById('phon-name');
+    phoneName.innerText = result.name;
+
+    const imag = document.getElementById('images');
+    imag.innerHTML = `
+    <img id="img" src="${result.image}" alt="">
+    `
+    const Barnd = document.getElementById('barnd');
+    Barnd.innerText = result.brand;
+
+    const storeg = document.getElementById('storeg');
+    storeg.innerText = result.mainFeatures.storage;
+    
+    const display = document.getElementById('displaysize');
+    display.innerText = result.mainFeatures.displaySize;
+
+    const chip = document.getElementById('chip');
+    chip.innerText = result.mainFeatures.chipSet;
+
+    const rams = document.getElementById('ram');
+    rams.innerText = result.mainFeatures.memory;
+
+    const title = document.getElementById('title');
+    title.innerText = result.slug;
+
+    const release = document.getElementById('release');
+    release.innerText = result.releaseDate;
+
+    const gps = document.getElementById('gps');
+    gps.innerText = result.others.GPS;
+}
+
+
+// show all button
+const showAll = () => {
+    searchBtn(true)
+}
+
+loadPhone()
